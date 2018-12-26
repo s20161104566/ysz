d4a6e22ca8763330c3e37ffd76d946198f9598aa
s20161104566 <a20161104566@mac04.local>
2018-12-26 15:21:21 +0800

    Initial Commit

---
+++ ysz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ysz
+//
+//  Created by 20161104566 on 2018/12/26.
+//  Copyright © 2018 20161104566. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
